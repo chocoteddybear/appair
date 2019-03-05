4be9d0d2fe049b881ba7909dc2f33c0a8d2ff465
Vova Vasilev <vova.vasilev@yahoo.com>
2019-03-05 12:47:31 +0300

    Create fecth in App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// HTTP request
+
+const path = "http://35.225.146.132:3000/api/chat/list";
+
+componentDidMount() {
+    const options = {
+      method: "GET",
+      body: JSON.stringify(item),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "b32f9a44-151b-46ed-8cba-1eaeb58360df",
+        "cache-control": "no-cache",
+      }),
+      mode: 'no-cors'
+    }
+
+    return fetch(path, options)
+      .then(response => response.json())
+      .then(list => {
+        this.setState({
+          list
+        });
+      });
+      .catch(error => console.error(`Error: ${error}`));
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: []
+    }
+  }
+
   render() {
+    console.log(this.state.list);
     return (
       <div className="App">
         <header className="App-header">
@@ -11,14 +45,6 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
